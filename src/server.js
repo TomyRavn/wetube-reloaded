bc76f25a0fc5498263b3251ade9418f151d3f56f
TomyRavn <godqwer123@naver.com>
2021-10-12 17:51:40 +0900

    Morgan_Import

--- src/server.js
+++ src/server.js
@@ -1,31 +1,14 @@
 import express from "express"; // == const express = require("express");
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
+const logger = morgan("dev");
 
 /////////////////////////////////////////////////////////////////////////
 //순서 : 미들웨어부터 먼저
-//(Middleware Function)
-const logger = (req, res, next) =>{
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    console.log("Allowed, you may continue.");
-    next();
-};
-
-//(Global Middleware)
 app.use(logger);
-app.use(privateMiddleware);
-
 
 //1. inline(Finalware)
 app.get("/", (req, res) => 
@@ -36,17 +19,13 @@ app.get("/", (req, res) =>
 
 //2. function(Finalware)
 const handleLogin = (req, res) => {
-    return res.send("Login here.");
+    return res.send("Login here."); 
 }
 
 app.get("/login", handleLogin);
 /////////////////////////////////////////////////////////////////////////
 
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the private lounge.");
-}
 
-app.get("/protected", handleProtected);
 
 /////////////////////////////////////////////////////////////////////////
 

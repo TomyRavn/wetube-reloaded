28ae0f75348a84d3894dca1830a17b235434b1ea
TomyRavn <godqwer123@naver.com>
2021-10-13 09:58:32 +0900

    Router

--- src/server.js
+++ src/server.js
@@ -10,6 +10,24 @@ const logger = morgan("dev");
 //순서 : 미들웨어부터 먼저
 app.use(logger);
 
+
+//Global Router
+const globalRouter = express.Router();
+const userRouter = express.Router();
+const videoRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+const handleEditUser = (req, res) => res.send("Edit User");
+
+globalRouter.get("/", handleHome);
+videoRouter.get("/watch", handleWatchVideo);
+userRouter.get("/edit", handleEditUser);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
 //1. inline(Finalware)
 app.get("/", (req, res) => 
     {

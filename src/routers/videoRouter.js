e1f353874f5a487e2619c55872eeb5d3249803ff
TomyRavn <godqwer123@naver.com>
2021-10-14 13:29:43 +0900

    regexp

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { watch } from "../controllers/videoController";
-import { edit } from "../controllers/videoController";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/upload", upload);
 
 
-export default videoRouter;
+export default videoRouter; 

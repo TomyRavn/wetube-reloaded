885d2bf423b4f95e0652eefe035e854b2dfa152c
TomyRavn <godqwer123@naver.com>
2021-10-15 14:14:18 +0900

    Template_Partial

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,6 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-    console.log(req.params);
-    return res.send(`Watch Video #${req.params.id}`);
-}
-export const edit = (req, res) => {
-    console.log(req.params);
-    return res.send("Edit");
-}
+export const trending = (req, res) => res.render("home", { pageTitle : "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle : "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle : "Edit" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -7,8 +7,17 @@ import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
+console.log(process.cwd());     //현재 작업 디렉토리 확인 ; 서버를 기동하는 파일 위치 : package.json의 위치
+
 const app = express();
 const logger = morgan("dev");
+
+/////////////////////////////////////////////////////////////////////////
+//뷰 엔진 설정
+const VIEW_ROUTE = "/src";
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + VIEW_ROUTE + "/views");
 
 /////////////////////////////////////////////////////////////////////////
 //순서 : 미들웨어부터 먼저

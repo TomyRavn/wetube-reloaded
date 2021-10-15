2866fa1b0a5d7b19c6bfa0b5221789c46e7480c1
TomyRavn <godqwer123@naver.com>
2021-10-15 16:37:49 +0900

    Mixin,Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,33 @@
-const fakeUser = {
-    username: "Nicolas",
-    loggedIn: false
+
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments:2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second Video",
+            rating: 4.2,
+            comments:1,
+            createdAt: "5 minutes ago",
+            views: 42,
+            id: 2,
+        },
+        {
+            title: "Third Video",
+            rating: 3,
+            comments:5,
+            createdAt: "10 minutes ago",
+            views: 5,
+            id: 3,
+        },
+    ];
+    return res.render("home", { pageTitle : "Home", videos });
 }
-
-
-export const trending = (req, res) => res.render("home", { pageTitle : "Home" , fakeUser : fakeUser});
 export const see = (req, res) => res.render("watch", { pageTitle : "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle : "Edit" });
 export const search = (req, res) => res.send("Search");

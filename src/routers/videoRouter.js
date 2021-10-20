dae75e5359c980faf89177aed68e2f78074b1f75
TomyRavn <godqwer123@naver.com>
2021-10-20 17:22:15 +0900

    schema,read

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,9 @@ import { watch, getUpload, postUpload, getEdit, postEdit } from "../controllers/
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
 
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 /** =======> 
  * videoRouter.get("/:id(\\d+)/edit", getEdit);
  * videoRouter.post("/:id(\\d+)/edit", postEdit);

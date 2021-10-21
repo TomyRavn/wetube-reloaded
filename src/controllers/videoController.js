6c62dccd49ee503963dc6245cbbd3e2818a74bba
TomyRavn <godqwer123@naver.com>
2021-10-21 17:19:10 +0900

    delete

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -88,6 +88,11 @@ export const postUpload = async (req, res) => {
             errorMessage: error._message,
         });
     }
-
    
+};
+export const deleteVideo = async (req, res) => {
+    const { id } = req.params;
+    await Video.findByIdAndDelete(id);
+
+    return res.redirect("/");
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { watch, getUpload, postUpload, getEdit, postEdit } from "../controllers/videoController";
+import { watch, getUpload, postUpload, getEdit, postEdit, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 /** =======> 
  * videoRouter.get("/:id(\\d+)/edit", getEdit);
  * videoRouter.post("/:id(\\d+)/edit", postEdit);

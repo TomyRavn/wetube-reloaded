de0b81793e0e13226f2ffb5a2520e17d81d6f9b5
TomyRavn <godqwer123@naver.com>
2021-10-25 09:49:37 +0900

    validation-1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,15 +2,49 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-    const { name, username, email, password, location } = req.body;
-    await User.create({
-      name,
-      username,
-      email,
-      password,
-      location,
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
     });
-    return res.redirect("/login");
+  }
+  ////////////////////////////////////////////////////////////////
+  //1. duplicate
+  // const usernameExists = await User.exists({ username });
+  // if (usernameExists) {
+  //   return res.render("join", {
+  //     pageTitle,
+  //     errorMessage: "This username is already taken.",
+  //   });
+  // }
+  // const emailExists = await User.exists({ email });
+  // if (emailExists) {
+  //   return res.render("join", {
+  //     pageTitle,
+  //     errorMessage: "This email is already taken.",
+  //   });
+  // }
+  //2. $or operator
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+  ///////////////////////////////////////////////////////////////
+
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");

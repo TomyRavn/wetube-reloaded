426304e0b0916340bec27ec496ac7b53b92c2c25
TomyRavn <godqwer123@naver.com>
2021-10-27 17:59:47 +0900

    Github_Login_Final

--- src/models/User.js
+++ src/models/User.js
@@ -2,15 +2,16 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true},
-    password: { type: String, required: true },
-    name: { type: String, required: true },
-    location: String,
+  email: { type: String, required: true, unique: true },
+  socialOnly: { type: Boolean, default: false },
+  username: { type: String, required: true, unique: true },
+  password: { type: String },
+  name: { type: String, required: true },
+  location: String,
 });
 
-userSchema.pre("save", async function(){
-    this.password = await bcrypt.hash(this.password, 5);
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 5);
 });
 
 const User = mongoose.model("User", userSchema);

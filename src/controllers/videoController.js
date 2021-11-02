8cc926c0629cb29b2d034a083ce44151b9e3bf18
TomyRavn <godqwer123@naver.com>
2021-11-02 10:27:56 +0900

    Video_Owner-1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from "../models/Video";
+import User from "../models/User";
 
 export const home = async (req, res) => {
   //1. call back
@@ -17,13 +18,13 @@ export const home = async (req, res) => {
 };
 export const watch = async (req, res) => {
   // const id = req.params.id;
-
   //ES6
   const { id } = req.params;
   const video = await Video.findById(id);
+  const owner = await User.findById(video.owner);
   if (!video)
     return res.status(404).render("404", { pageTitle: "Video not found." });
-  return res.render("watch", { pageTitle: video.title, video });
+  return res.render("watch", { pageTitle: video.title, video, owner });
 };
 export const getEdit = async (req, res) => {
   const { id } = req.params;
@@ -67,6 +68,9 @@ export const postUpload = async (req, res) => {
   //=> ES6
   const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
+  const {
+    user: { _id },
+  } = req.session;
 
   //1. Save
   // const video = new Video({
@@ -89,6 +93,7 @@ export const postUpload = async (req, res) => {
       //fileUrl: file.path,
       //=> ES6
       fileUrl,
+      owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
--- src/models/Video.js
+++ src/models/Video.js
@@ -10,6 +10,7 @@ const videoSchema = new mongoose.Schema({
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
   },
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
 //※ Mongoose Middleware 는 반드시 Model 생성 전에 생성

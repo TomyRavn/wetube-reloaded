863cc69613892a79b490c1bb368b4ff4caa38a14
TomyRavn <godqwer123@naver.com>
2021-11-02 16:22:29 +0900

    Webpack_and_Config_update

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,1 +1,3 @@
-alert("hi");
+import "../scss/styles.scss";
+
+alert("it works!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/client/js/main.js",
   mode: "development",
+  watch: true,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"), //path.resolve => add path with separator
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"), //path.resolve => add path with separator
+    clean: true,
     // path: "./assets/js",     //error : not absolute path
     //console.log(__dirname);   //this directory path
   },
@@ -19,6 +27,14 @@ module.exports = {
             presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
         },
+      },
+      {
+        test: /\.scss$/,
+        //마지막 동작하는 loader부터(웹팩의 loader는 역순으로 동작)
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        //1. sass-loader : scss->css
+        //2. css-loader : translate @import,url()
+        //3. style-loader : css->DOM =======> MiniCssExtractPlugin : create file parted
       },
     ],
   },

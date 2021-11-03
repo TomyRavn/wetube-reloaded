56bca429dcdc6f436e05c54fbe519f6445a9f41c
TomyRavn <godqwer123@naver.com>
2021-11-03 10:51:28 +0900

    Style-End

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -252,7 +252,14 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id).populate("videos");
+  //double populate
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
